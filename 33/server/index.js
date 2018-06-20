678ef394f57a31def2da31f0050a68eeaefd473d
Akinfeev <40438315+Akinfeev@users.noreply.github.com>
2018-06-20 20:44:40 +0200

    Add files via upload

---
+++ 33/server/index.js
@@ -0,0 +1,40 @@
+var wss = require("ws").Server;
+
+var server = new wss({port : 591});
+
+var clients = new Set();
+var messages = [];
+
+var fs = require("fs");
+fs.readFile("messages.txt", {flag:"a+"}, function(err, content) {
+    content;
+    console.log(content.toString());
+    messages.push(content.toLocaleString());
+
+});
+
+server.on("connection", function(socket) {
+    clients.add(socket);
+    for(var m of messages) {
+        socket.send(m);
+    }
+
+    socket.on("message", function(message) {
+      messages.push(message);
+
+      s ="";
+   for(var m of messages) {
+            s = m+ '\n' + s;
+        }
+        fs.writeFile("messages.txt",  s);
+
+
+ for(var inter of clients) {
+    inter.send(message);
+  }
+    });
+
+    socket.on("close", function ( ){
+        clients.delete(socket);
+    });
+});
